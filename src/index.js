a2539d2358163b86bcae26ce6a549a332c2e0f3c
seohyeonKK <sts05158@gmail.com>
2021-10-11 01:26:07 +0900

    Redux Introduction

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const ADD = "ADD"
+const MINUS = "MINUS"
+
+number.innerText = 0;
+
+// reducer
+// if-else 보다 switch case 사용
+const countModifier = (count = 0, action) => {
+  switch (action.type){
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+}
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
+
+// state 변할 때마다 onChange 실행
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({type:ADD})
+}
+
+const handleMinus = () => {
+  countStore.dispatch({type:MINUS})
+}
+
+add.addEventListener("click", handleAdd)
+minus.addEventListener("click", handleMinus)
+
+
+
